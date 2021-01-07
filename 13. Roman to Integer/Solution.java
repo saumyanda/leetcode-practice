06fd5335340e3a578ed7be78bb5412051fae7916
Saumya Shandilya <saumyanda@gmail.com>
2021-01-07 09:46:13 -0800

    Create Solution.java

---
+++ 13. Roman to Integer/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int romanToInt(String s) {
+        
+        if(s==null || s.length()==0)
+            return 0;
+        
+       Map<Character,Integer> roman=new HashMap<Character,Integer>();
+        
+        roman.put('I',1);
+        roman.put('V',5);
+        roman.put('X',10);
+        roman.put('L',50);
+        roman.put('C',100);
+        roman.put('D',500);
+        roman.put('M',1000);
+        
+        int result=roman.get(s.charAt(s.length()-1));
+        
+       for(int i=s.length()-2;i>=0;i--)
+       {
+           if(roman.get(s.charAt(i)) >= roman.get(s.charAt(i+1)))
+           {
+               result=result+roman.get(s.charAt(i));
+           }
+           else
+           {
+               result=result-roman.get(s.charAt(i));
+           }
+       }
+        
+        return result;
+        
+    }
+}
