072f8d00316f102fd0937d55c6bb69a20d08d0be
Saumya Shandilya <saumyanda@gmail.com>
2021-05-24 15:11:00 -0700

    Update Solution.java

--- 1. Two Sum/Solution.java
+++ 1. Two Sum/Solution.java
@@ -1,21 +1,23 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result=new int[2];
         
-        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+        Map<Integer,Integer> numbers=new HashMap<>();
+        
+        int[] result=new int[2];
         
         for(int i=0;i<nums.length;i++)
         {
-            if(map.containsKey(nums[i]))
+            if(numbers.containsKey(target-nums[i]))
             {
-                result[0]=map.get(nums[i]);
-                result[1]=i;
+                result[0]=i;
+                result[1]=numbers.get(target-nums[i]);
             }
             else
             {
-                map.put(target-nums[i],i);
+                numbers.put(nums[i],i);
             }
         }
+        
         
         return result;
     }
