2587e3e869c202e716168ff09460fbebcc290a30
Saumya Shandilya <saumyanda@gmail.com>
2021-05-30 13:15:12 -0700

    Update Solution.java

--- 443. String Compression/Solution.java
+++ 443. String Compression/Solution.java
@@ -1,9 +1,16 @@
 class Solution {
     public int compress(char[] chars) {
-      
-        int index=0;
-        int i=0,j=0;
         
+        int i=0, j=0;
+        int index=0;
+       
+        //2 pointer i and j
+        //i for each group of chars
+        //j for counting occurence of each character
+        //count of each character = j-i
+        //Move j ahead till the next character is different from chars[i]
+//If number of occurences of character is (j-i) > 1: then add the count after the character e.g. a 12 would be a "1" "2"
+        //Move i ahead to the next character group i=j
         while(i<chars.length)
         {
             while(j<chars.length && chars[j]==chars[i])
@@ -11,22 +18,23 @@ class Solution {
                 j++;
             }
             
-            chars[index++]=chars[i];
+            //modify the array inplace to show the result
+            chars[index]=chars[i];
+            index++;
             
-            if(j-i>1)
+            if(j-i > 1)
             {
-                String s=j-i+"";
-                
-                for(int k=0;k<s.length();k++)
+                String count=j-i+"";
+                for(int k=0;k<count.length();k++)
                 {
-                    chars[index++]=s.charAt(k);
+                    chars[index]=count.charAt(k);
+                    index++;
                 }
             }
             
-            i=j;
+            i=j;  
         }
         
         return index;
-      
     }
 }
