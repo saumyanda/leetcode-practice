0723120af98fd2b8c16b6d4d9fe9ada58f854174
Saumya Shandilya <saumyanda@gmail.com>
2021-06-20 20:40:11 -0700

    Update Solution.java

--- 121. Best Time to Buy and Sell Stock/Solution.java
+++ 121. Best Time to Buy and Sell Stock/Solution.java
@@ -1,14 +1,27 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int minPrice=Integer.MAX_VALUE;
-        int maxProfit=0;
+        /*
+        The sentence "We need to find the largest peak following the smallest valley" is plain wrong. 
+        What we are actually doing is this: for every element, 
+        we are calculating the difference between that element and the minimum of all the values before that element 
+        and we are updating the maximum profit if the difference thus found is greater than the current maximum profit.
+        */
+        int maxprofit=0;
+        int minprice=Integer.MAX_VALUE;
         
         for(int i=0;i<prices.length;i++)
         {
-            minPrice=Math.min(minPrice, prices[i]);
-            maxProfit=Math.max(maxProfit, prices[i]-minPrice);
+            if(prices[i] < minprice)
+            {
+                minprice=prices[i];
+            }
+            else if(prices[i]-minprice > maxprofit)
+            {
+                maxprofit=prices[i]-minprice;
+            }
         }
         
-        return maxProfit;
+        return maxprofit;
+        
     }
 }
