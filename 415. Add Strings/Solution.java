9a9b9481577c6a947e4f0944da9aa33b3ba52b91
Saumya Shandilya <saumyanda@gmail.com>
2021-06-27 21:54:58 -0700

    Update Solution.java

--- 415. Add Strings/Solution.java
+++ 415. Add Strings/Solution.java
@@ -1,31 +1,49 @@
 class Solution {
+   
     public String addStrings(String num1, String num2) {
+        
+        //Initialize an empty res structure. Start from carry = 0
+        StringBuilder res=new StringBuilder();
+        
+    //Set a pointer at the end of each string: i = num1.length() - 1, j = num2.length() - 1
         int i=num1.length()-1;
         int j=num2.length()-1;
-        StringBuilder result=new StringBuilder();
+        
         int carry=0;
+
+        //Loop over the strings from the end to the beginning using i and j 
+        //Stop when both strings are used entirely.
         while(i>=0 || j>=0)
         {
-          int sum=carry;
+            //Set x to be equal to a digit from string nums1 at index i. 
+            //If i has reached the beginning of nums1, set x to 0
+            int x= i>=0 ? num1.charAt(i)-'0' : 0;
             
-          if(i>=0)
-          {
-              sum+=num1.charAt(i--)-'0';
-          }
-          if(j>=0)
-          {
-              sum+=num2.charAt(j--)-'0';
-          }
-            result.append(sum%10);
-            carry=sum/10;
+            //Do the same for y. 
+            //Set y to be equal to digit from string nums2 at index j. 
+            //If j has reached the beginning of nums2, set y to 0.
+            int y= j>=0 ? num2.charAt(j)-'0' : 0;
+            
+            
+            //Compute the current value: value = (x1 + x2 + carry) % 10, 
+            //and update the carry: carry = (x1 + x2 + carry) / 10
+            int value=(x+y+carry)%10;
+            carry=(x+y+carry)/10;
+            
+            //Append the current value to the result: res.append(value)
+            res.append(""+value);
+            i--;
+            j--;
         }
-        
+       
+        //Now both strings are done. 
+        //If the carry is still non-zero, update the result: res.append(carry)
         if(carry>0)
         {
-            result.append(carry);
+            res.append(carry);
         }
         
-        return result.reverse().toString();
-        
+        //Reverse the result, convert it to a string, and return that string
+        return res.reverse().toString();
     }
 }
