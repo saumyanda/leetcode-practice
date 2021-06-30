405919a9a07f24f42cc33a836969a5c19de647cf
Saumya Shandilya <saumyanda@gmail.com>
2021-06-29 19:34:24 -0700

    Update Solution.java

--- 235. Lowest Common Ancestor of a Binary Search Tree/Solution.java
+++ 235. Lowest Common Ancestor of a Binary Search Tree/Solution.java
@@ -9,18 +9,23 @@
  */
 
 class Solution {
+    //The function assumes that both p and q are present in BST
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
-public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-    
-    if(p.val<root.val && q.val>root.val)
+         // If both p and q are smaller than root, then LCA lies in left
+        if(p.val<root.val && q.val<root.val)
+        {
+            root=lowestCommonAncestor(root.left,p,q);
+        }
+        
+         // If both p and q are larger than root, then LCA lies in right
+        if(p.val>root.val && q.val>root.val)
+        {
+            root=lowestCommonAncestor(root.right,p,q);
+        }
+        
+        //If both the above cases are false then return the current node as LCA
         return root;
-   
-    if(p.val<root.val && q.val<root.val)
-     return lowestCommonAncestor(root.left,p,q);
-    
-    if(p.val>root.val && q.val>root.val)
-    return lowestCommonAncestor(root.right,p,q);
-    
-    return root;
-  }  
+        
+    }
 }
