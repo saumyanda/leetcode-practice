065a61b3577005cf0ddbc4532dc322eecb539188
Saumya Shandilya <saumyanda@gmail.com>
2021-07-28 00:50:16 -0700

    Create Solution1.java

---
+++ 48. Rotate Image/Solution1.java
@@ -0,0 +1,34 @@
+class Solution {
+  /*
+  The most elegant solution for rotating the matrix is 
+  to firstly reverse the matrix around the main diagonal, 
+  and then reverse it from left to right. 
+  These operations are called transpose and reflect in linear algebra.
+  */
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reflect(matrix);
+    }
+    
+    public void transpose(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+    }
+    
+    public void reflect(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+}
