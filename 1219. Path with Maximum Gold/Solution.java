a62a951eb6a383ccd411b7d012b4100217ce510d
Saumya Shandilya <saumyanda@gmail.com>
2021-09-25 18:07:00 -0700

    Create Solution.java

---
+++ 1219. Path with Maximum Gold/Solution.java
@@ -0,0 +1,60 @@
+class Solution {
+    
+    private final int[] deltaRows={-1,0,1,0};
+    private final int[] deltaCols={0,1,0,-1};
+    
+    public int getMaximumGold(int[][] grid) {
+       
+        int maxgold=0;
+        int sum=0;
+        int rows=grid.length;
+        int cols=grid[0].length;
+        
+        boolean[][] visited=new boolean[rows][cols];
+        
+        for(int r=0;r<rows;r++)
+        {
+            for(int c=0;c<cols;c++)
+            {
+                maxgold=Math.max(maxgold,dfs(grid,r,c,sum,visited));
+            }
+        }
+        
+        return maxgold;
+    }
+    
+    public int dfs(int[][] grid, int r, int c, int sum, boolean[][] visited)
+    {
+        //if current cell r and c is out of bounds from the grid or 
+        //if the current cell has 0 gold, don't visit and return result
+        if(r<0 || r>=grid.length || c<0 || c>=grid[0].length || grid[r][c]==0)
+        {
+            return sum;
+        }
+        
+        //if visited don't visit the cell again
+        if(visited[r][c]==true)
+        {
+            return sum;
+        }
+        
+        int maxgold=0;
+        //take the gold in the cell
+        //visit it
+        sum+=grid[r][c];
+        visited[r][c]=true;
+        
+        //do a dfs on the neighbors
+        //traverse it's 4 neigbors to get max value
+        for(int i=0;i<deltaRows.length;i++)
+        {
+            maxgold=Math.max(maxgold,dfs(grid,r+deltaRows[i],c+deltaCols[i],sum,visited));
+        }
+        
+        //backtrack
+        sum-=grid[r][c];
+        visited[r][c]=false;
+        
+        return maxgold;
+    }
+}
