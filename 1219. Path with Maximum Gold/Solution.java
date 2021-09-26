9435194403228315dec92bf91cc9bda4a372a017
Saumya Shandilya <saumyanda@gmail.com>
2021-09-25 18:09:57 -0700

    Update Solution.java

--- 1219. Path with Maximum Gold/Solution.java
+++ 1219. Path with Maximum Gold/Solution.java
@@ -1,5 +1,10 @@
 class Solution {
     
+    /*
+    Loop through the grid, for each element at (i, j) perform DFS to get the max value;
+if (i, j) is out of the bound, or grid[i][j] == 0, or visited already, return current sum;
+Otherwise, recurse to the neighbors and accumulate the sum.
+    */
     private final int[] deltaRows={-1,0,1,0};
     private final int[] deltaCols={0,1,0,-1};
     
@@ -11,7 +16,7 @@ class Solution {
         int cols=grid[0].length;
         
         boolean[][] visited=new boolean[rows][cols];
-        
+        //Loop through the grid, for each element at (i, j) perform DFS to get the max value
         for(int r=0;r<rows;r++)
         {
             for(int c=0;c<cols;c++)
@@ -26,7 +31,7 @@ class Solution {
     public int dfs(int[][] grid, int r, int c, int sum, boolean[][] visited)
     {
         //if current cell r and c is out of bounds from the grid or 
-        //if the current cell has 0 gold, don't visit and return result
+        //if the current cell has 0 gold, don't visit and return sum
         if(r<0 || r>=grid.length || c<0 || c>=grid[0].length || grid[r][c]==0)
         {
             return sum;
