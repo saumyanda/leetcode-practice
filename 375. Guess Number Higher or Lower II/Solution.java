42da502dcde41cde8865a5bb31a52fe514094b06
Saumya Shandilya <saumyanda@gmail.com>
2021-09-30 13:31:34 -0700

    Update Solution.java

--- 375. Guess Number Higher or Lower II/Solution.java
+++ 375. Guess Number Higher or Lower II/Solution.java
@@ -1,52 +1,53 @@
-We are playing the Guessing Game. The game will work as follows:
+//https://leetcode.com/problems/guess-number-higher-or-lower-ii/discuss/84778/Recursion-%2B-Memization
 
-I pick a number between 1 and n.
-You guess a number.
-If you guess the right number, you win the game.
-If you guess the wrong number, then I will tell you whether the number I picked is higher or lower, and you will continue guessing.
-Every time you guess a wrong number x, you will pay x dollars. If you run out of money, you lose the game.
-Given a particular n, return the minimum amount of money you need to guarantee a win regardless of what number I pick.
+/*
+Let's take an instance, for n = 3, we have 3 choices either to choose 1 or 2 or 3.
+Let's say we choose 1. There are 2 possible chances,
 
- 
+[Case X]: 1 is the actual number so you pay 0$ or,
+[Case Y]: 1 is not the actual number so you pay 1$ (now you know that the actual number is > 1 because for every guess we will know if its less than or greater than, in our case it can only be greater than) and have the subproblem (2, 3). To choose from (2, 3) again recursively applying the same method, you can choose either 2 or 3. If you pick 2, you have 2 possible outcomes again. 2 is the actual number and you pay 0$ for this choice or 2 is not the actual number and you pay 2$ for this choice and you know 3 is the answer since that's the only one left. On the other hand, if you had picked 3, then either 3 is correct or you pay 3$ and know 2 is the actual answer since it's the only one left. So to sum up this, you pay 2$ in the worst case if you choose 2 or pay 3$ in the worst case if you pick 3$. So we will pick the min of the worst cases which is 2$ and hence 2 is the answer for (2, 3) subproblem. (Notice the minimax? ;) ) So, the total cost paid in this is 1$ + 2$ = 3$.
+Let's say you picked 2 initially. You have 2 possible outcomes.
 
-Example 1:
-
-
-Input: n = 10
-Output: 16
-Explanation: The winning strategy is as follows:
-- The range is [1,10]. Guess 7.
-    - If this is my number, your total is $0. Otherwise, you pay $7.
-    - If my number is higher, the range is [8,10]. Guess 9.
-        - If this is my number, your total is $7. Otherwise, you pay $9.
-        - If my number is higher, it must be 10. Guess 10. Your total is $7 + $9 = $16.
-        - If my number is lower, it must be 8. Guess 8. Your total is $7 + $9 = $16.
-    - If my number is lower, the range is [1,6]. Guess 3.
-        - If this is my number, your total is $7. Otherwise, you pay $3.
-        - If my number is higher, the range is [4,6]. Guess 5.
-            - If this is my number, your total is $7 + $3 = $10. Otherwise, you pay $5.
-            - If my number is higher, it must be 6. Guess 6. Your total is $7 + $3 + $5 = $15.
-            - If my number is lower, it must be 4. Guess 4. Your total is $7 + $3 + $5 = $15.
-        - If my number is lower, the range is [1,2]. Guess 1.
-            - If this is my number, your total is $7 + $3 = $10. Otherwise, you pay $1.
-            - If my number is higher, it must be 2. Guess 2. Your total is $7 + $3 + $1 = $11.
-The worst case in all these scenarios is that you pay $16. Hence, you only need $16 to guarantee a win.
-Example 2:
-
-Input: n = 1
-Output: 0
-Explanation: There is only one possible number, so you can guess 1 and not have to pay anything.
-Example 3:
-
-Input: n = 2
-Output: 1
-Explanation: There are two possible numbers, 1 and 2.
-- Guess 1.
-    - If this is my number, your total is $0. Otherwise, you pay $1.
-    - If my number is higher, it must be 2. Guess 2. Your total is $1.
-The worst case is that you pay $1.
- 
-
-Constraints:
-
-1 <= n <= 200
+2 is the actual number and you pay 0$ or,
+2 is not the actual number and you pay 2$. At this point, you get to know if the actual number is less than or greater than the actual number. So, you will know the answer right away without another guess. So you end up paying 2$.
+So, if you choose 2 initially, you risk paying 2$ at most.
+Similarly, if you had chosen 3 initially, you risk paying 4$ at most. Hence picking 2 initially is the best option and you risk at most 2$.
+This leads to a natural recursion, which you can find in the code below. I have memoized it in a matrix.
+*/
+class Solution {
+    
+    int[][] memo;
+    
+    public int getMoneyAmount(int n) {
+        
+        memo=new int[n+1][n+1];
+        
+        for(int[] row: memo)
+        {
+            Arrays.fill(row,Integer.MAX_VALUE);
+        }
+        
+        return solve(1, n);
+    }
+    
+    private int solve(int l, int r)
+    {
+        if(l>=r)
+        {
+            return 0;
+        }
+        
+        //if memo contains the calculated amount
+        if(memo[l][r]!=Integer.MAX_VALUE)
+        {
+            return memo[l][r];
+        }
+        
+        for(int i=l;i<=r;i++)
+        {
+            memo[l][r]=Math.min(memo[l][r],Math.max(i+solve(l,i-1), i+solve(i+1,r)));
+        }
+        
+        return memo[l][r];
+    }
+}
