e4e5e01605a49aa037bc8f1bbac732000d471482
Saumya Shandilya <saumyanda@gmail.com>
2021-10-02 13:40:34 -0700

    Create Solution.java

---
+++ 1769. Minimum Number of Operations to Move All Balls to Each Box/Solution.java
@@ -0,0 +1,43 @@
+class Solution {
+    //https://youtu.be/TjedLBZbqmM
+    public int[] minOperations(String boxes) {
+        
+        int n=boxes.length();
+        
+        int[] result=new int[n];
+        
+        int ahead=0; //number of balls ahead of a position
+        int behind=0; //number of balls behind a position
+        
+        for(int i=0;i<n;i++)
+        {
+            if(boxes.charAt(i)=='1')
+            {
+                ahead++;
+        //add the number of moves it will take to bring all balls in array to position 0
+                result[0]+=i; 
+            }
+        }
+        
+        //if at index 0 there are balls present
+        //then the number of balls ahead of that position would decrement
+        //and the number of balls behind that position would increment
+        if(boxes.charAt(0)=='1')
+        {
+            ahead--;
+            behind++;
+        }
+        
+        for(int i=1;i<n;i++)
+        {
+            result[i]+=result[i-1]-ahead+behind;
+            if(boxes.charAt(i)=='1')
+            {
+               ahead--;
+               behind++;
+            }
+        }
+
+        return result;
+    }
+}
