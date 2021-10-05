afd5933fe8a0560ef80118aeea95bb4fdd795134
Saumya Shandilya <saumyanda@gmail.com>
2021-10-04 18:55:27 -0700

    Create Solution.java

---
+++ 340. Longest Substring with At Most K Distinct Characters/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int windowStart=0;
+        int max=Integer.MIN_VALUE;
+        
+        HashMap<Character,Integer> counts=new HashMap<>();
+        
+        for(int windowEnd=0; windowEnd<s.length(); windowEnd++)
+        {
+            char rightChar=s.charAt(windowEnd);
+//keep adding one character in the sliding window (i.e., slide the window ahead) in a stepwise fashion
+            counts.put(rightChar, counts.getOrDefault(rightChar,0)+1);
+            
+//shrink the window from the beginning if the count of distinct characters in the HashMap is larger than K
+            while(counts.size()>k) //shrink the window till it gets to size k
+            {
+                char leftChar=s.charAt(windowStart);
+                counts.put(leftChar,counts.get(leftChar)-1);
+//While shrinking, we’ll decrement the character’s frequency going out of the window and remove it from the HashMap if its frequency becomes zero.
+                if(counts.get(leftChar)==0)
+                {
+                    counts.remove(leftChar);
+                }
+                windowStart++;
+            }
+//check if the current window length is the longest so far, and if so, remember its length
+            max=Math.max(max,windowEnd-windowStart+1);
+        }
+        
+        return max==Integer.MIN_VALUE ? 0 : max;
+    }
+}
