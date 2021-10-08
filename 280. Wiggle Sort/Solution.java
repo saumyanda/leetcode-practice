3db116a8080273c9a0e538155b9484c3d7ae7f16
Saumya Shandilya <saumyanda@gmail.com>
2021-10-07 23:59:38 -0700

    Update Solution.java

--- 280. Wiggle Sort/Solution.java
+++ 280. Wiggle Sort/Solution.java
@@ -10,27 +10,29 @@ class Solution {
     */
     public void wiggleSort(int[] nums) {
         
-        for(int i=0;i<nums.length-1;i++)
+        for(int i=1;i<nums.length;i++)
         {
             if(i%2==0)
             {
-                if(nums[i]>nums[i+1])
+                if(nums[i]>nums[i-1])
                 {
-                   int temp=nums[i+1];
-                   nums[i+1]=nums[i];
-                   nums[i]=temp;
+                   swap(nums,i-1,i);
                 }
             }
             else
             {
-                if(nums[i]<nums[i+1])
+                if(nums[i]<nums[i-1])
                 {
-                   int temp=nums[i+1];
-                   nums[i+1]=nums[i];
-                   nums[i]=temp;
+                   swap(nums,i-1,i);
                 }
             }
         }  
     }
     
+    private static void swap(int[] nums, int i, int j)
+    {
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
 }
